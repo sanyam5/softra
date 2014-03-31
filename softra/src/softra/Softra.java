e1ceacb6ac332cd3d410d079ab343a342d27bc3c
Arkanath Pathak <pathak.arkanath@gmail.comZ>
2014-03-31 19:22:25 +0530

    ss

---
+++ softra/src/softra/Softra.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package softra;
+
+/**
+ *
+ * @author arkanathpathak
+ */
+public class Softra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
