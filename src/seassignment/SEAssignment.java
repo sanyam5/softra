fc8d6fb33052f04e24ea0402dca02d7cc3c1f36a
Arkanath Pathak <pathak.arkanath@gmail.comZ>
2014-03-31 18:30:06 +0530

    shuruat

---
+++ src/seassignment/SEAssignment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package seassignment;
+
+/**
+ *
+ * @author arkanathpathak
+ */
+public class SEAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
