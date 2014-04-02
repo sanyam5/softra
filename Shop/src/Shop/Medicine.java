9abc516301a95b07d3a58edc499b63c7d5f9921a
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2014-04-02 22:14:46 +0530

    made medicine template

--- Shop/src/Shop/Medicine.java
+++ Shop/src/Shop/Medicine.java
@@ -6,12 +6,73 @@
 
 package Shop;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author arkanathpathak
  */
 public class Medicine {
     private String codenumber;
+    private String tradename;
+    private ArrayList<Vendor> supplyvendors;
+    private ArrayList<MedicineBatch> batches;
+    
+    
+    public void sell(long quantity)
+    {
+        
+    }
+    
+    public ArrayList<MedicineBatch> getExpiredbatches()
+    {
+        return batches;//wrong code
+    }
+    
+    public void removeExpired()
+    {
+        
+    }
+    public boolean addSupply(MedicineBatch medicineBatchToAdd)
+    {
+        return true;//wrong code
+    }
+    public ArrayList<Medicine> getTobeordered()
+    {
+        return new ArrayList<Medicine>();
+    }
+
+    public String getTradename() {
+        return tradename;
+    }
+
+    public ArrayList<Vendor> getSupplyvendors() {
+        return supplyvendors;
+    }
+
+    public ArrayList<MedicineBatch> getBatches() {
+        return batches;
+    }
+
+    public long getUnitsellingprice() {
+        return unitsellingprice;
+    }
+
+    public long getPurchasingprice() {
+        return purchasingprice;
+    }
+
+    public long getTotalsold() {
+        return totalsold;
+    }
+
+    public long getTotalstock() {
+        return totalstock;
+    }
+    private long unitsellingprice;
+    private long purchasingprice;
+    private long totalsold;
+    private long totalstock;
 
     public String getCodenumber() {
         return codenumber;
