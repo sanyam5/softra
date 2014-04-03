558095c408e2571660f0e44f24b5c8a78596ef88
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2014-04-03 17:14:43 +0530

    made gettobveordred

--- Shop/src/Shop/Medicine.java
+++ Shop/src/Shop/Medicine.java
@@ -28,6 +28,7 @@ public class Medicine {
     private long purchasingprice;
     private long totalsold;
     private long totalstock;
+    private Timestamp addDate;
 
     public Medicine(String tradename,
             ArrayList<Vendor> supplyvendors, 
@@ -154,9 +155,27 @@ public class Medicine {
     {
         return true;
     }
-    public ArrayList<Medicine> getTobeordered()
+    public ArrayList<Medicine> getTobeordered() throws SQLException
     {
-        return new ArrayList<Medicine>();
+        ArrayList<Medicine> tobeordered =  new ArrayList<Medicine>();
+        if (dbInit.conn == null)
+        {
+            dbInit.startDb();
+        }
+        PreparedStatement pst = dbInit.conn.prepareStatement("SELECT * FROM medsales");
+        ResultSet rs = pst.executeQuery();
+        while(rs.next())
+        {            
+            String codenumber = rs.getString(1);
+            Medicine toAddCandy = new Medicine(codenumber);
+            long elapsedTime = (new Date()).getTime() - toAddCandy.addDate.getTime();
+            long elapsedWeeks = elapsedTime/(7*24*3600*1000);
+            if(elapsedWeeks==0) continue;
+            if(totalstock*elapsedWeeks <= totalsold)
+                tobeordered.add(toAddCandy);
+        }
+        return tobeordered;
+        
     }
 
     public String getTradename() {
