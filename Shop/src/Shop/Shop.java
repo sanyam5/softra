134d08266f1aef155b33f9eda6f66a5e3b9c4974
Arkanath Pathak <pathak.arkanath@gmail.comZ>
2014-04-05 14:44:31 +0530

    Merge origin/master
    Conflicts:
    	Shop/dbname/db.lck
    	Shop/derby.log
    	Shop/src/Shop/MainPage.java

--- Shop/src/Shop/Shop.java
+++ Shop/src/Shop/Shop.java
@@ -116,7 +116,7 @@ public class Shop {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, ParseException, Exception {
-        dbInit.startDb();
+
         dbInit.refreshAll();
         dbInit.printMedicines();
         dbInit.printMedSales();
@@ -124,18 +124,12 @@ public class Shop {
         System.out.println(a.getTotalstock());
         new AccountsInfo();
         dbInit.printVendors();
-        
-        long id1 = new Vendor("zzaari Baag", "ahekubabu").getId();
-        long id2 = new Vendor("azaari Baag", "zhekubabu").getId();
+        dbInit.printMedBatches();
+//        long id1 = new Vendor("zzaari Baag", "ahekubabu").getId();
+//        long id2 = new Vendor("azaari Baag", "zhekubabu").getId();
 //        a.sell(2, new java.util.Date().getTime());
 //        new Timestamp
 //        new Login();
-
-////        Medicine a = new Medicine("MED1");
-////        System.out.println(a.getTotalstock());
-////        new AccountsInfo();
-////        a.sell(2, new java.util.Date().getTime());
-
 //        Shop.Register(new ShopOwner("Arkanath", "pass", "B-121", "981821", "x@y.com", new Timestamp(new java.util.Date().getTime())));
 //        dbInit.startDb();
 //        ShopOwner a = new ShopOwner();
